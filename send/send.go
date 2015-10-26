f3a8ecee6d53d3f797f0f49ca5aad701563dc016
Kyle Kloepper <kyle@defend7.com>
2015-10-26 12:24:20 -0500

    Adding send application.

---
+++ send/send.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"os"
+	"strings"
+
+	"github.com/kloepper/learn_travis_ci/message"
+)
+
+func main() {
+	text := strings.Join(os.Args[1:len(os.Args)], " ")
+	m := message.Prepare(text)
+	message.Send(m)
+}
